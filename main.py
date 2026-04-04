4d3bb65f9ade4ad73d6db37b0a5d1effbb55dfd5
grassblock <i@gb0.dev>
2026-04-04 15:02:05 +0800

    feat: add event handler

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.core.star.filter.command import CommandFilter
+from astrbot.core.star.filter.command_group import CommandGroupFilter
+
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -10,15 +12,26 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.regex(r"^/")
+    async def block_non_command_slash_message(self, event: AstrMessageEvent):
+        """拦截 / 开头但未命中任何指令的消息，终止事件传播。"""
+        activated_handlers = event.get_extra("activated_handlers", [])
+        has_real_command = False
+
+        for handler in activated_handlers:
+            if handler.handler_full_name.endswith("_block_non_command_slash_message"):
+                continue
+
+            if any(
+                isinstance(f, CommandFilter | CommandGroupFilter)
+                for f in handler.event_filters
+            ):
+                has_real_command = True
+                break
+
+        if not has_real_command:
+            event.stop_event()
+            return
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
